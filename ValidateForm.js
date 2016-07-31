61e2b0fc00876dde08aac52a1914b4eb59db405e
ninja25538 <ninja_william@hotmail.com>
2016-07-31 16:38:40 -0400

    Update ValidateForm.js

--- ValidateForm.js
+++ ValidateForm.js
@@ -1,85 +1,17 @@
-function Submit(){
- var emailRegex = /^[A-Za-z0-9._]*\@[A-Za-z]*\.[A-Za-z]{2,5}$/;
- var fname = document.form.Name.value,
-  lname = document.form.LastName.value,
-  femail = document.form.Email.value,
-  freemail = document.form.enterEmail.value,
-  fpassword = document.form.Password.value,
-  fmonth = document.form.birthday_month.value,
-  fday = document.form.birthday_day.value,
-  fyear = document.form.birthday_year.value;
-   
- if( fname == "" )
-   {
-     document.form.Name.focus() ;
-  document.getElementById("errorBox").innerHTML = "enter the first name";
-     return false;
-   }
- if( lname == "" )
-   {
-     document.form.LastName.focus() ;
-   document.getElementById("errorBox").innerHTML = "enter the last name";
-     return false;
-   }
-    
-   if (femail == "" )
- {
-  document.form.Email.focus();
-  document.getElementById("errorBox").innerHTML = "Enter an Email";
-  return false;
-  }else if(!emailRegex.test(femail)){
-  document.form.Email.focus();
-  document.getElementById("errorBox").innerHTML = "enter the valid email";
-  return false;
-  }
-   
-   if (freemail == "" )
- {
-  document.form.enterEmail.focus();
-  document.getElementById("errorBox").innerHTML = "Re-enter the email";
-  return false;
-  }else if(!emailRegex.test(freemail)){
-  document.form.enterEmail.focus();
-  document.getElementById("errorBox").innerHTML = "Re-enter the valid email";
-  return false;
-  }
-   
-  if(freemail !=  femail){
-   document.form.enterEmail.focus();
-   document.getElementById("errorBox").innerHTML = "emails are not matching, re-enter again";
-   return false;
-   }
-   
-   
- if(fpassword == "")
-  {
-   document.form.Password.focus();
-   document.getElementById("errorBox").innerHTML = "enter the password";
-   return false;
-  }
-   
-   if (fmonth == "") {
-        document.form.birthday_month.focus();
-  document.getElementById("errorBox").innerHTML = "select the birthday month";
-        return false;
-     }
-  if (fday == "") {
-        document.form.birthday_day.focus();
-  document.getElementById("errorBox").innerHTML = "select the birthday day";
-        return false;
-     }
-  if (fyear == "") {
-        document.form.birthday_year.focus();
-  document.getElementById("errorBox").innerHTML = "select the birthday year";
-        return false;
-     }
-  if(document.form.radiobutton[0].checked == false && document.form.radiobutton[1].checked == false){
-    document.getElementById("errorBox").innerHTML = "select your gender";
-    return false;
-   }
-  if(fname != '' && lname != '' && femail != '' && freemail != '' && fpassword != '' && fmonth != '' && fday != '' && fyear != ''){
-   document.getElementById("errorBox").innerHTML = "form submitted successfully";
-   }
-     
+function checkPassword(){
+ var password = pass.value;
+ if (password.length < 8) {
+  passMsg.className = "waring";
+  passMsg.textContent = "Not long enough";
+ } else {
+  passMsg.textContent = " ";
+ }
 }
-- See more at: http://www.webcodehelpers.com/2013/11/registration-form-validation-using-javascript-with-example.html#sthash.TSfD5pDQ.dpuf
+function tipPassword(){
+ passMssg.className = "tip";
+ passMsg.innerHTML = "Password must be at least 8 characters";
+}
+var pass = document.getElementByClassName("input_password");
+var passMsg = document.getElementByClassName("feedback");
+pass.addEventListener("focus", checkPassword, false);
+pass.addEventListener("blur", tipPassword, false); 
