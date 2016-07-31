319bdfea3c0de7ba6481cc56817d681101363963
ninja25538 <ninja_william@hotmail.com>
2016-07-31 17:02:14 -0400

    Update ValidateForm.js

--- ValidateForm.js
+++ ValidateForm.js
@@ -1,17 +1,16 @@
-var pass = document.getElementById("input_password");
-var passMsg = document.getElementById("feedback");
-
-function checkPassword(){
- var password = pass.value;
- if (password.length < 8) {
-  passMsg.textContent = "Not long enough";
- } else {
-  passMsg.textContent = " ";
+var el = document.getElementById("input_password");
+var elMsg = document.getElementById("feedback");
+function checkPassword() {
+ var pasword = el.value;
+ if (password.length >= 8) {
+  elMsg.textContent = "Not long enough...";
+ } else { 
+  elMsg.textContent = " ";
  }
 }
-function tipPassword(){
- passMsg.innerHTML = "Password must be at least 8 characters";
+function tipPassword() {
+ elMsg.innerHtml "Username must be at least 8 characters";
 }
 
-pass.addEventListener("focus", checkPassword, false);
-pass.addEventListener("blur", tipPassword, false); 
+el.addEventListener("focus", tipPassword, false);
+el.addEventListener("blur", checkPassword, false);
