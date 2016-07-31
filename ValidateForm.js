6b3badb0828226d3178ec0fcc9fc0ecbf88e6b92
ninja25538 <ninja_william@hotmail.com>
2016-07-31 17:17:22 -0400

    Update ValidateForm.js

--- ValidateForm.js
+++ ValidateForm.js
@@ -1,6 +1,8 @@
 var el = document.getElementById("input_password");
 var elMsg = document.getElementById("feedback");
 function password(){
+ if (el < 8){
  elMsg.innerText = "Not long enough yet...";
+ }
 }
 el.addEventListener("focus", password, false);
