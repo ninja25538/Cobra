bc7b733985d7ee09d401419ad5a9f439c6db1745
ninja25538 <ninja_william@hotmail.com>
2016-07-31 16:50:15 -0400

    Update ValidateForm.js

--- ValidateForm.js
+++ ValidateForm.js
@@ -1,3 +1,6 @@
+var pass = document.getElementById("input_password");
+var passMsg = document.getElementById("feedback");
+
 function checkPassword(){
  var password = pass.value;
  if (password.length < 8) {
@@ -9,8 +12,6 @@ function checkPassword(){
 function tipPassword(){
  passMsg.innerHTML = "Password must be at least 8 characters";
 }
-var pass = document.getElementById("input_password");
-var passMsg = document.getElementById("feedback");
 
 pass.addEventListener("focus", checkPassword, false);
 pass.addEventListener("blur", tipPassword, false); 
