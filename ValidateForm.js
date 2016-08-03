eacd39be5df32e92f5c9c41f9cce832e45d48b7f
ninja25538 <ninja_william@hotmail.com>
2016-08-03 16:56:16 -0400

    Update ValidateForm.js

--- ValidateForm.js
+++ ValidateForm.js
@@ -1,8 +1,9 @@
 var Msg = document.getElementById("Msg");
 function Submit(){
-    var x = document.forms["form"]["First_Name"]["Email"].value;
-    if (x == null || x == "") {
-        Msg.textContent = "Please fill out all the information correctly!";
+    var a = document.forms["form"]["First_Name"].value;
+    var b = document.forms["form"]["Last_Name"]
+    if (a == null || a == "" || b == null || b == "") {
+        Msg.textContent = "Please fill out all information correctly!";
         return false;
     } else {
       Msg.textContent = "Registration succesful!"
