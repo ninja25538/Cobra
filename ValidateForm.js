04ff3c21b4cbaa1853a2d9f5e79b2e3996782dde
ninja25538 <ninja_william@hotmail.com>
2016-08-03 08:44:15 -0400

    Update ValidateForm.js

--- ValidateForm.js
+++ ValidateForm.js
@@ -1,5 +1,4 @@
 function Submit(){
-function validateForm() {
     var x = document.forms["form"]["input_username"]["input_password"]["input_first_name"]["input_last_name"]["input_email"].value;
     if (x == null || x == "") {
         Msg.textContent = "Please fill out all the information correctly!";
