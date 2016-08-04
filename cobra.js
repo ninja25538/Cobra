679361959f994b7e6c2971dbec792a3d16fd785b
ninja25538 <ninja_william@hotmail.com>
2016-08-04 18:46:40 -0400

    Update cobra.js

--- cobra.js
+++ cobra.js
@@ -4,9 +4,6 @@ function Submit(){
     var b = document.forms["form"]["Last_Name"].value;
     var c = document.forms["form"]["Username"].value;
     var d = document.forms["form"]["Password"].value;
-    var x = document.forms["form"]["Email"].value;
-    var atpos = x.indexOf("@");
-    var dotpos = x.lastIndexOf(".");
     if (a == null || a == "" || b == null || b == "" || c == "" || c == null || d == "" || d == null) {
         Msg.textContent = "Please fill out all information correctly!";
         return false;
@@ -15,6 +12,10 @@ function Submit(){
       window.location.replace("Home");
     }
 }
+    var x = document.forms["form"]["Email"].value;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+
 if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length){
     Msg.textContent = "INCORRECT EMAIL!"
 }
