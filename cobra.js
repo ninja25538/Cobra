f1696a300425abf330626b852a0d98d509cf635a
ninja25538 <ninja_william@hotmail.com>
2016-08-04 19:17:26 -0400

    Update cobra.js

--- cobra.js
+++ cobra.js
@@ -5,13 +5,11 @@ function Submit(){
     var c = document.forms["form"]["Username"].value;
     var d = document.forms["form"]["Password"].value;
     var x = document.forms["form"]["Email"].value;
-    var month = document.forms["form"]["birthday_month"].value;
-    var day = document.forms["form"]["birthday"].value;
     var year = document.forms["form"]["birthday_year"].value;
     var atpos = x.indexOf("@");
     var dotpos = x.lastIndexOf(".");
 
-    if (a == null || a == "" || b == null || b == "" || c == "" || c == null || d == "" || d == null || atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length || month =="Month" || birthday == "Day" || year == "Year") {
+    if (a == null || a == "" || b == null || b == "" || c == "" || c == null || d == "" || d == null || atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length || year == "Year") {
         Msg.textContent = "Please fill out all information correctly!";
         return false;
     } else {
